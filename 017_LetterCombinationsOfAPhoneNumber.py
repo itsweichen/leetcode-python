21e715016ac5bacc10c579ac6e8d768c8e0a842c
Weichen Liu <kevbtc@gmail.com>
2016-04-02 22:39:41 -0700

    04/02/2016

--- 017_LetterCombinationsOfAPhoneNumber.py
+++ 017_LetterCombinationsOfAPhoneNumber.py
@@ -1,15 +1,16 @@
-# 03/29/2016
+# sol_1 - 03/29/2016 - intuitive(iteration)
+# sol_2 - 04/02/2016 - recursion
 
-# O(?), 52ms [9%]
-# initing res without judging each time doesn't improve much
-
-class Solution(object):
+class Solution_1(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
         
+        # O(?), 52ms [9%]
+        # initing res without judging each time doesn't improve much
+
         if digits == "" or digits is None:
             return []
         
@@ -38,3 +39,48 @@ class Solution(object):
                 res = temp
             
         return res
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        # O(?), 44 ms [36%]
+
+
+        if len(digits) == 0:
+            return []
+        
+        mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        
+        result = []
+        
+        self.helper("", digits, result, mapping)
+        
+        return result
+        
+        
+    def helper(self, soFar, rest, result, mapping):
+        # base case
+        if len(rest) == 0:
+            result.append(soFar)
+            
+        else:
+            curDigit = rest[0]
+            
+            if curDigit in mapping:
+                for letter in mapping[curDigit]:
+                    self.helper(soFar + letter, rest[1:], result, mapping)
+            
+        
+            
