20de80718b214ded0296ec4bf0d06b61fd0d6f9b
Weichen Liu <kevbtc@gmail.com>
2016-05-20 22:09:04 -0700

    #28

---
+++ 028_ImplementStrStr.py
@@ -0,0 +1,66 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        # 5/20/2016
+        # 68 ms [20%]
+        # function call: 204 ms
+
+
+        if needle == "":
+            return 0
+        if haystack == "":
+            return -1
+
+        n = len(haystack)
+        m = len(needle)
+
+        for i in range(n):
+            if haystack[i] == needle[0]:
+                # don't make it a function call here.
+                ok = True
+                if n - i >= m:
+                    for j in range(m):
+                        if haystack[i+j] != needle[j]:
+                            ok = False
+                            break
+                else:
+                    ok = False #!!!
+                if ok:
+                    return i
+        return -1
+
+    def strStr2(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        if needle == "":
+            return 0
+        if haystack == "":
+            return -1
+
+        n = len(haystack)
+        m = len(needle)
+
+        for i in range(n):
+            if haystack[i] == needle[0]:
+                ok = self.checkNeedle(haystack[i:], needle, m)
+                if ok:
+                    return i
+        return -1
+
+    def checkNeedle(self, sp, needle, m):
+        if len(sp) >= m:
+            for j in range(m):
+                if sp[j] != needle[j]:
+                    return False
+            return True
+        else:
+            return False
