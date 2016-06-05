5c7f2693d7831f235935883ffec08a322f57df23
Weichen Liu <kevbtc@gmail.com>
2016-06-04 21:04:59 -0700

    #141

---
+++ 141_LinkedListCycle.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        #1 use hash map
+        # 92 ms [35%]
+        """
+        cur = head
+        visited = {}
+        while cur is not None:
+            if cur in visited:
+                return True
+            else:
+                visited[cur] = 0
+            cur = cur.next
+        return False
+        """
+
+        #2 w/o extra space
+
+        # Can I do this? lol
+        # 88 ms [50%]
+
+        if head is None:
+            return False
+
+        cur = head.next
+
+        while cur is not None:
+            if cur is head:
+                return True
+            prev = cur
+            cur = cur.next
+            prev.next = head
+        return False
